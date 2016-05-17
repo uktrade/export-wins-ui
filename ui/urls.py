1902ae9b3db33c8564ecc2686442727ceff21efe
Daniel Quinn <code@danielquinn.org>
2016-05-17 16:35:02 +0100

    Added route for customer responses

--- ui/urls.py
+++ ui/urls.py
@@ -2,12 +2,21 @@ from django.conf.urls import url
 
 from ui.views import IndexView
 from users.views import LoginView, LogoutView
-from wins.views import NewWinView, ThanksView
+from wins.views import NewWinView, ThanksView, CustomerResponseView
 
 urlpatterns = [
-    url(r"^wins/new", NewWinView.as_view(), name="new"),
-    url(r"^wins/thanks", ThanksView.as_view(), name="thanks"),
+
+    url(r"^wins/new/", NewWinView.as_view(), name="new"),
+    url(r"^wins/thanks/", ThanksView.as_view(), name="thanks"),
+    url(
+        r"^wins/review/(?P<win_id>[a-z0-9\-]+)/",
+        CustomerResponseView.as_view(),
+        name="responses"
+    ),
+
     url(r'^accounts/login/', LoginView.as_view(), name="login"),
     url(r"^accounts/logout/", LogoutView.as_view(), name="logout"),
+
     url(r"^$", IndexView.as_view(), name="index"),
+
 ]
