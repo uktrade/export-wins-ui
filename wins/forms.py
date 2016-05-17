b4f485b0649a61bc38c32c619b2e92fac917e271
Daniel Quinn <code@danielquinn.org>
2016-05-17 16:35:29 +0100

    Ironed-out breadkwons and advisors and added a link for customer responses

--- wins/forms.py
+++ wins/forms.py
@@ -6,6 +6,7 @@ from dateutil.relativedelta import relativedelta
 from django import forms
 from django.conf import settings
 from django.core.mail import send_mail
+from django.core.urlresolvers import reverse
 
 from alice.helpers import rabbit, get_form_field
 from alice.metaclasses import ReflectiveFormMetaclass
@@ -45,6 +46,9 @@ class WinForm(BootstrappedForm, metaclass=WinReflectiveFormMetaclass):
         label="HVO Specialist Involvement"
     )
 
+    class Meta(object):
+        exclude = ("id",)
+
     def __init__(self, *args, **kwargs):
 
         self.request = kwargs.pop("request")
@@ -55,6 +59,8 @@ class WinForm(BootstrappedForm, metaclass=WinReflectiveFormMetaclass):
 
         self._add_breakdown_fields()
         self._add_advisor_fields()
+
+        self._advisors = []
 
     def clean_date(self):
         date = self.cleaned_data.get("date")
@@ -78,29 +84,29 @@ class WinForm(BootstrappedForm, metaclass=WinReflectiveFormMetaclass):
 
         win = self.push(settings.WINS_AP, self.cleaned_data)
 
-        regex = re.compile("^breakdown_(?P<type>non_exports|exports)_(?P<year>\d+)$")
-        for name, value in self.cleaned_data.items():
-            m = regex.match(name)
-            if m:
-                pass
-        self.push(settings.BREAKDOWNS_AP, 7)
-        self.push(settings.ADVISORS_AP, 7)
+        for data in self._get_breakdown_data(win["id"]):
+            self.push(settings.BREAKDOWNS_AP, data)
 
-        raise Exception("Don't Panic")
+        for data in self._get_advisor_data(win["id"]):
+            self.push(settings.ADVISORS_AP, data)
 
         # Send mail
         send_mail(
             "Subject Line!",
-            "Message!",
+            "Oh hai! You should click this:\n\n  {}".format(
+                self.request.build_absolute_uri(
+                    reverse("responses", kwargs={"win_id": win["id"]})
+                )
+            ),
             settings.SENDING_ADDRESS,
             (self.cleaned_data["customer_email_address"],)
         )
 
-    def _save_breakdown(self):
-        pass
-
     def push(self, ap, data):
 
+        # The POST request is http-url-encoded rather than json-encoded for now
+        # since I don't know how to set it that way and don't have the time to
+        # find out.
         response = rabbit("post", ap, data=data, request=self.request)
 
         # This would be a nice place to use a public key to encrypt the POST
@@ -109,12 +115,6 @@ class WinForm(BootstrappedForm, metaclass=WinReflectiveFormMetaclass):
         if not response.status_code == 201:
             raise forms.ValidationError(
                 "Something has gone terribly wrong.  Please contact support.")
-
-        import json
-        print(
-            json.dumps(response.json(), indent=2, sort_keys=True),
-            response.status_code
-        )
 
         return response.json()
 
@@ -136,6 +136,24 @@ class WinForm(BootstrappedForm, metaclass=WinReflectiveFormMetaclass):
                     )
                 )
 
+    def _get_breakdown_data(self, win_id):
+
+        r = []
+        now = datetime.utcnow()
+
+        for i in range(0, 5):
+            d = now + relativedelta(years=i)
+            for t in ("exports", "non_exports"):
+                value = self.cleaned_data.get("breakdown_{}_{}".format(t, i))
+                if value:
+                    r.append({
+                        "type": "1" if t == "exports" else "2",
+                        "year": d.year,
+                        "value": value,
+                        "win": win_id
+                    })
+        return r
+
     def _add_advisor_fields(self):
 
         schema = rabbit("get", settings.ADVISORS_AP + "schema/").json()
@@ -149,8 +167,12 @@ class WinForm(BootstrappedForm, metaclass=WinReflectiveFormMetaclass):
                     "class": "form-control"
                 })
 
-    @staticmethod
-    def _test_at_least_one_advisor(cleaned_data):
+    def _get_advisor_data(self, win_id):
+        for advisor in self._advisors:
+            advisor["win"] = win_id
+        return self._advisors
+
+    def _test_at_least_one_advisor(self, cleaned_data):
         """
         A convoluted way to test whether one advisor was filled out or not.
         This could probably be a lot cleaner but deadlines.
@@ -167,8 +189,13 @@ class WinForm(BootstrappedForm, metaclass=WinReflectiveFormMetaclass):
                     break
                 score += 1
             if score == 4:
-                return True
-        return False
+                self._advisors.append({
+                    "name": self.cleaned_data["advisor_{}_name".format(i)],
+                    "team_type": self.cleaned_data["advisor_{}_team_type".format(i)],
+                    "hq_team": self.cleaned_data["advisor_{}_hq_team".format(i)],
+                    "location": self.cleaned_data["advisor_{}_location".format(i)]
+                })
+        return bool(self._advisors)
 
 
 class ConfirmationForm(BootstrappedForm, metaclass=ConfirmationFormMetaclass):
