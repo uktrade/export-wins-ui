7ddfbc773a57ae03fc80ae95a18e22acdf6f68cc
Graham Veal <grahamveal@gmail.com>
2017-03-12 10:26:50 +0000

    Add component

---
+++ ui/static/ui/js/components/ew.components.WinDate.js
@@ -0,0 +1,51 @@
+
+ew.components.WinDate = (function( $, CustomEvent ){
+
+	var isDate = /^[0-9]{2}\/[0-9]{4}/;
+
+	function errorMessage( field ){
+		return ( field + 'is required for WinDateComponent' );
+	}
+	
+	function WinDateComponent( opts ){
+
+		if( !opts ){ throw new Error( errorMessage( 'opts' ) ); }
+		if( !opts.id ){ throw new Error( errorMessage( 'opts.id' ) ); }
+
+		this.events = {
+			yearChange: new CustomEvent()
+		};
+
+		this.$input = $( '#' + opts.id );
+		this.val = this.$input.val();
+
+		this.$input.on( 'keyup', $.proxy( this.handleInput, this ) );
+		this.$input.on( 'change', $.proxy( this.handleInput, this ) );
+	}
+
+	WinDateComponent.prototype.handleInput = function( e ){
+
+		var oldVal = this.val;
+		var newVal = this.$input.val();
+		
+		if( newVal !== oldVal && isDate.test( newVal ) ){
+
+			this.val = newVal;
+			this.checkYear( oldVal, newVal );
+		}
+	};
+
+	WinDateComponent.prototype.checkYear = function( oldDate, newDate ){
+	
+		var oldYear = oldDate.substr( 3 );
+		var newYear = newDate.substr( 3 );
+
+		if( oldYear !== newYear ){
+
+			this.events.yearChange.publish( newYear );
+		}		
+	};
+
+	return WinDateComponent;
+
+}( jQuery, ew.CustomEvent ));
