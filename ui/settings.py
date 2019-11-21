fd6d8b4e255428dd0397734e53b10af922e07314
Reupen Shah <reupen@users.noreply.github.com>
2019-11-21 11:46:05 +0000

    Improve logging configuration
    This enables more detailed logging when DEBUG=False. 
    
    The same configuration as Data Hub API has been used.

--- ui/settings.py
+++ ui/settings.py
@@ -58,6 +58,34 @@ MIDDLEWARE_CLASSES = [
 
 ROOT_URLCONF = 'ui.urls'
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'root': {
+        'level': 'INFO',
+        'handlers': ['console'],
+    },
+    'formatters': {
+        'verbose': {
+            'format': '%(asctime)s [%(levelname)s] [%(name)s] %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'ERROR',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+    },
+}
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
