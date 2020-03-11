e2c78cacb2056536b68247e88026675d76a7f2c9
Mark Higham <mark.higham@gmail.com>
2020-03-11 17:40:32 +0000

    work in progress - getting to know this repo

--- alice/braces.py
+++ alice/braces.py
@@ -1,3 +1,4 @@
+import logging
 from urllib.parse import urlparse, urlunparse
 
 from django.conf import settings
@@ -8,27 +9,31 @@ from django.utils.encoding import force_text
 
 from alice.helpers import rabbit
 
+logger = logging.getLogger(__name__)
+
 
 class LoginRequiredMixin(object):
-    """
-    Just like django.contrib.auth.mixins.LoginRequiredMixin, but with the
-    majority of the overridable stuff in that class stripped out.
-
-    This is only here because attempting to import that class causes an
-    explosion since the file it lives in imports things that are
-    database-dependent.
-    """
-
     def dispatch(self, request, *args, **kwargs):
 
+        logger.debug(f"using alice_id {request.alice_id}")
+
+        logger.debug(f"testing whether user is logged in at {settings.IS_LOGGED_IN_AP}")
         response = rabbit.get(settings.IS_LOGGED_IN_AP, request=request)
+        logger.debug(f"{settings.IS_LOGGED_IN_AP} retured {response.status_code}")
+
         is_logged_in = response.json()
-        if not request.alice_id or not is_logged_in:
-            return self.redirect_to_login(
-                self.request.get_full_path(),
-                force_text(settings.LOGIN_URL)
-            )
-        return super(LoginRequiredMixin, self).dispatch(request, *args, **kwargs)
+
+        if is_logged_in and request.alice_id:
+            logger.debug(f"user is logged in and alice_id is found")
+            return super(LoginRequiredMixin, self).dispatch(request, *args, **kwargs)
+
+        # TODO - Grab SSO login url from backend and redirect to there.
+
+        return self.redirect_to_login(
+            self.request.get_full_path(), force_text(settings.LOGIN_URL)
+        )
+
+        logger.debug(f"not logged in")
 
     def redirect_to_login(self, next, login_url=None):
         """
@@ -39,6 +44,11 @@ class LoginRequiredMixin(object):
         login_url_parts = list(urlparse(resolved_url))
         querystring = QueryDict(login_url_parts[4], mutable=True)
         querystring[REDIRECT_FIELD_NAME] = next
-        login_url_parts[4] = querystring.urlencode(safe='/')
+        login_url_parts[4] = querystring.urlencode(safe="/")
+
+        redirect_url = urlunparse(login_url_parts)
+        logger.debug(f"login redirect url is {redirect_url}")
 
         return HttpResponseRedirect(urlunparse(login_url_parts))
+
+
