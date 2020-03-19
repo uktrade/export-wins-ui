03b0e5d43e9936f017f52c47afb43e972593aba6
Mark Higham <mark.higham@gmail.com>
2020-03-19 12:08:03 +0000

    removing old username/password project assets

--- users/views.py
+++ users/views.py
@@ -4,15 +4,21 @@ from datetime import datetime
 import jwt
 from django.conf import settings
 from django.shortcuts import render
-from django.utils.http import is_safe_url
 from django.urls import reverse
-from django.views.generic import FormView, RedirectView
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 
 from alice.helpers import rabbit
-from .forms import LoginForm, get_dataserver_session_cookie
 
 logger = logging.getLogger(__name__)
+
+
+def get_dataserver_session_cookie(cookies):
+    """ Get the session cookie from the data server """
+
+    for cookie in cookies:
+        if cookie.name == "sessionid":
+            return cookie
+    raise Exception("The data server didn't return a session cookie")
 
 
 def cookie_domain():
@@ -90,61 +96,3 @@ def oauth_logout_view(request):
 def oauth_logged_out_view(request):
     logger.info("logged out")
     return render(request, "users/logged_out.html", {})
-
-
-class LoginView(FormView):
-    """ Login via data server and set JWT cookie
-
-    When user put their details into the form, the form passes them to the data
-    server, which creates and manages a session for the user.
-
-    THIS IS LEFT IN THE CODE TO HANDLE MIGRATION FROM USERNAME/PASSWORD TO SSO
-
-    """
-
-    form_class = LoginForm
-    template_name = "users/login.html"
-
-    def form_valid(self, form):
-        response = FormView.form_valid(self, form)
-
-        # After successful login, save dict of user data given by data server
-        # API into JWT cookie. Also save session id of session from data
-        # server, for use when making requests to data server via Rabbit.
-        # Note UI server and admin server have different secrets and domains.
-        logger.debug(form.user)
-        jwt_val = jwt.encode(
-            {"user": form.user, "session": form.session_cookie.value},
-            settings.COOKIE_SECRET,
-            "HS256",
-        ).decode("utf-8")
-        expires = datetime.fromtimestamp(form.session_cookie.expires).strftime(
-            "%a, %d %b %Y %H:%M:%S"
-        )
-        kwargs = {
-            "value": jwt_val,
-            "expires": expires,
-            "secure": settings.SESSION_COOKIE_SECURE,
-            "httponly": True,
-            "domain": cookie_domain(),
-        }
-
-        response.set_cookie("alice", **kwargs)
-        return response
-
-    def get_success_url(self):
-        redirect_to = self.request.GET.get("next")
-        if redirect_to:
-            if is_safe_url(url=redirect_to, allowed_hosts=self.request.get_host()):
-                return redirect_to
-        return "/"
-
-
-class LogoutView(RedirectView):
-    url = "/"
-
-    def get(self, request, *args, **kwargs):
-        rabbit.get(settings.LOGOUT_AP, request=request)  # Data server log out
-        response = RedirectView.get(self, request, *args, **kwargs)
-        response.delete_cookie("alice", domain=cookie_domain())
-        return response
